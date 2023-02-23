42c0ded2953f54b8641e0acacd73b0fd800be920
RJMBol <r.j.m.bol@tudelft.nl>
2023-02-23 14:28:27 +0100

    ignore data files

---
+++ count-lines.py
@@ -0,0 +1,11 @@
+""" This module counts the number of lines in standard input
+Input: strings from the system's standard input
+"""
+
+import sys
+
+count = 0
+for line in sys.stdin:
+	count += 1
+
+print(count, 'lines in standard input')
